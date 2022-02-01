1a46eee3b71e719b4f55184539ccfa0953ec5a06
Никита <@LAPTOP-GBI2EGAC>
2022-02-01 10:51:45 +0500

    Добавьте файлы проекта.

---
+++ 4.6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _4._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество элементов N:");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Исходный масив:");
+            int[] a = new int[N];
+            Random r = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                a[i] = r.Next(1, 10);
+                Console.Write(a[i] + "\t");
+            }
+            Console.WriteLine("\t");
+            int x = 0;
+            Console.WriteLine("Изменённый масив:");
+            for (int i = 1; i < N; i++)
+            {
+
+            }
+        }
+    }
+}
